6de98ade0dc268d70947d167d6d3a281cd3b0408
rownakabdullahomi <rownakabdullahomi@gmail.com>
2025-01-06 03:39:39 +0600

    jwt token created

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const app = express();
 const cors = require('cors');
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
-
 
 
 
@@ -53,14 +53,14 @@ async function run() {
       res.send(result);
     })
 
-    app.delete("users/:id", async (req, res) => {
+    app.delete("/users/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await userCollection.deleteOne(query);
       res.send(result);
     })
 
-    app.patch("users/admin/:id", async (req, res) => {
+    app.patch("/users/admin/:id", async (req, res) => {
       const id = req.params.id;
       const query = {_id : new ObjectId(id)};
       const updatedDoc = {
