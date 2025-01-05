e30bcb7dbf07c2c3733506fa0cde2147402554ac
rownakabdullahomi <rownakabdullahomi@gmail.com>
2025-01-06 05:09:34 +0600

    admin check done

--- index.js
+++ index.js
@@ -35,21 +35,27 @@ async function run() {
 
 
     // jwt related api
-    app.post("/jwt", async(req, res) => {
+    app.post("/jwt", async (req, res) => {
       const user = req.body;
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: "1d"})
-      res.send({token});
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "1d" })
+      res.send({ token });
     })
 
     // middlewares
     const verifyToken = (req, res, next) => {
-      if(!req.headers.authorization){
-        return res.status(401).send({message: "forbidden access"});
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: "forbidden access" });
       }
 
       const token = req.headers.authorization.split(" ")[1]
-      
-      
+
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: "forbidden access" });
+        }
+        req.decoded = decoded;
+        next();
+      })
       // next();
     }
 
@@ -58,6 +64,20 @@ async function run() {
     app.get("/users", verifyToken, async (req, res) => {
       const result = await userCollection.find().toArray();
       res.send(result);
+    })
+
+    app.get("/users/admin/:email", verifyToken, async (req, res) => {
+      const email = req.params.email;
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: "unauthorized access" })
+      }
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      let admin = false;
+      if(user){
+        admin = user?.role === "admin";
+      }
+      res.send({admin});
     })
 
     app.post("/users", async (req, res) => {
@@ -82,7 +102,7 @@ async function run() {
 
     app.patch("/users/admin/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id : new ObjectId(id)};
+      const query = { _id: new ObjectId(id) };
       const updatedDoc = {
         $set: {
           role: "admin"
