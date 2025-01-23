263798b1edfb5dc33f21a156bbd9db47e6db279b
rownakabdullahomi <rownakabdullahomi@gmail.com>
2025-01-24 01:40:00 +0600

    payment intent created

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const cors = require('cors');
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
 
 const port = process.env.PORT || 5000;
 // middleware
@@ -58,6 +60,7 @@ async function run() {
       })
       // next();
     }
+
 
     const verifyAdmin = async (req, res, next) => {
       const email = req.decoded.email;
@@ -194,6 +197,24 @@ async function run() {
       const result = await cartCollection.deleteOne(query);
       res.send(result);
     })
+
+
+     // payment intent
+     app.post('/create-payment-intent', async (req, res) => {
+      const { price } = req.body;
+      const amount = parseInt(price * 100);
+      console.log(amount, 'amount inside the intent')
+
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: 'usd',
+        payment_method_types: ['card']
+      });
+
+      res.send({
+        clientSecret: paymentIntent.client_secret
+      })
+    });
 
 
 
