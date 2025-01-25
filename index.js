327bd4476af32fe4005d2a663dd192ff736fd1b0
rownakabdullahomi <rownakabdullahomi@gmail.com>
2025-01-25 18:29:43 +0600

    payment continue

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ async function run() {
     const menuCollection = client.db("bistroDB").collection("menu");
     const reviewsCollection = client.db("bistroDB").collection("reviews");
     const cartCollection = client.db("bistroDB").collection("carts");
+    const paymentCollection = client.db("bistroDB").collection("payments");
 
 
     // jwt related api
@@ -215,6 +216,23 @@ async function run() {
         clientSecret: paymentIntent.client_secret
       })
     });
+
+    app.post('/payments', async (req, res) => {
+      const payment = req.body;
+      const paymentResult = await paymentCollection.insertOne(payment);
+
+      //  carefully delete each item from the cart
+      console.log('payment info', payment);
+      const query = {
+        _id: {
+          $in: payment.cartIds.map(id => new ObjectId(id))
+        }
+      };
+
+      const deleteResult = await cartCollection.deleteMany(query);
+
+      res.send({ paymentResult, deleteResult });
+    })
 
 
 
