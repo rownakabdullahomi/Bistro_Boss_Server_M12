6fecddaf14f70cb70a7a1311f4c6e54a809149c8
rownakabdullahomi <rownakabdullahomi@gmail.com>
2025-02-01 02:34:26 +0600

    vercel deploy complete

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const userCollection = client.db("bistroDB").collection("users");
     const menuCollection = client.db("bistroDB").collection("menu");
@@ -322,8 +322,8 @@ async function run() {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -347,3 +347,5 @@ app.listen(port, () => {
   console.log(`Boss is coming on port ${port}`);
 })
 
+// live link
+// bistro-boss-server-eta-ashy.vercel.app
