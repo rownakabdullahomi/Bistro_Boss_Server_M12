b9296cf5fa033a4127fbb103c09f68ecfeaed7d3
rownakabdullahomi <rownakabdullahomi@gmail.com>
2025-02-01 01:51:43 +0600

    added

--- index.js
+++ index.js
@@ -275,7 +275,7 @@ async function run() {
     })
 
     // order-stats
-    app.get("/order-stats", async (req, res) => {
+    app.get("/order-stats",  verifyToken, verifyAdmin, async (req, res) => {
       const result = await paymentCollection.aggregate([
         {
           $unwind: "$menuItemIds"
@@ -301,6 +301,14 @@ async function run() {
             revenue: {
               $sum: "$menuItems.price"
             }
+          }
+        },
+        {
+          $project: {
+            _id: 0,
+            category: "$_id",
+            quantity: "$quantity",
+            revenue: "$revenue"
           }
         }
       ]).toArray();
